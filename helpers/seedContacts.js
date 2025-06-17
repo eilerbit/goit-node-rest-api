a533b285a72b0c9250c8b83e47d624bf71d7b46d
Anton Povar <povar.anton@gmail.com>
2025-06-18 00:41:24 +0300

    Add seeder

---
+++ helpers/seedContacts.js
@@ -0,0 +1,27 @@
+﻿import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import Contact from "../models/contactModel.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const SHOULD_SEED = true;
+
+export async function seedContactsIfNeeded() {
+    const count = await Contact.count();
+    if (count === 0 && SHOULD_SEED) {
+        const filePath = path.resolve(__dirname, "../db/contacts.json");
+        const json = await fs.readFile(filePath, "utf-8");
+        const rawContacts = JSON.parse(json);
+
+        const contacts = rawContacts.map(({ id, ...rest }) => ({
+            ...rest,
+            favorite: false,
+        }));
+
+        await Contact.bulkCreate(contacts);
+        console.log("✅ Seeded contacts from contacts.json (generated ids)");
+    }
+}
